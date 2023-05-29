22a19d9cbd7d403c028f544c4ba4c72538c18488
MKarpovski <alex1016yakovenko@gmail.com>
2023-05-29 16:21:02 -0400

    help

---
+++ vit/src/components/icons/payroll.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="name">
+    <h2>{{ payroll.firstname }}</h2>
+    <h3>{{ id }}</h3>
+  </div>
+</template>
+
+<script setup>
+const props = defineProps({
+  payroll: Object,
+  id: Number
+})
+</script>
+
+<style scoped></style>
--- vit/src/views/HomeView.vue
+++ vit/src/views/HomeView.vue
@@ -1,7 +1,38 @@
 <template>
-  <div></div>
+  <div class="container">
+    <PayrollName
+      v-for="(firstname, index) in payroll"
+      :key="payroll.firstname"
+      :id="index + 1"
+      :payroll="firstname"
+    />
+  </div>
 </template>
 
-<script setup></script>
+<script setup>
+import { ref, onMounted } from 'vue'
+import PayrollName from '../components/paroll.vue'
+const payroll = ref('')
 
-<style lang="scss" scoped></style>
+async function getPayroll() {
+  let res = await fetch('https://data.cityofnewyork.us/resource/k397-673e.json')
+
+  let data = await res.json()
+  payroll.values = data.results
+}
+onMounted(() => {
+  getPayroll()
+})
+</script>
+
+<style scoped>
+.conatainer {
+  width: 80vw;
+  margin: 30px auto;
+  display: flex;
+  flex-wrap: wrap;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-around;
+}
+</style>
