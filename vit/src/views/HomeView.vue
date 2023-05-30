31399b5929e501137d271fbdb0e1a79c48bb035c
MKarpovski <alex1016yakovenko@gmail.com>
2023-05-30 18:55:10 -0400

    i think i got it

--- vit/src/views/HomeView.vue
+++ vit/src/views/HomeView.vue
@@ -1,38 +1,24 @@
 <template>
-  <div class="container">
-    <PayrollName
-      v-for="(firstname, index) in payroll"
-      :key="payroll.firstname"
-      :id="index + 1"
-      :payroll="firstname"
-    />
-  </div>
+  <div></div>
 </template>
 
 <script setup>
 import { ref, onMounted } from 'vue'
-import PayrollName from '../components/paroll.vue'
-const payroll = ref('')
 
-async function getPayroll() {
-  let res = await fetch('https://data.cityofnewyork.us/resource/k397-673e.json')
+const inmateid = ref('')
+
+async function getinmateid() {
+  let res = await fetch(
+    'https://data.cityofnewyork.us/resource/6teu-xtgp.json?$query=SELECT%0A%20%20%60inmateid%60%2C%0A%20%20%60admitted_dt%60%2C%0A%20%20%60discharged_dt%60%2C%0A%20%20%60race%60%2C%0A%20%20%60gender%60%2C%0A%20%20%60inmate_status_code%60%2C%0A%20%20%60top_charge%60'
+  )
 
   let data = await res.json()
-  payroll.values = data.results
+
+  inmateid.values = data.results
 }
 onMounted(() => {
-  getPayroll()
+  getinmateid()
 })
 </script>
 
-<style scoped>
-.conatainer {
-  width: 80vw;
-  margin: 30px auto;
-  display: flex;
-  flex-wrap: wrap;
-  flex-direction: row;
-  align-items: center;
-  justify-content: space-around;
-}
-</style>
+<style scoped></style>
